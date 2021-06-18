b8016420568cd16680ff6c971d68e85b1b5d8bcd
Boilin-T <84787128+Boilin-T@users.noreply.github.com>
2021-06-18 14:41:18 +0200

    copied original to github

--- src/botb.js
+++ src/botb.js
@@ -1,1 +1,26 @@
-alert("Hello Fan!");
+function enterHere() {
+  let human = prompt("Are you new?");
+
+  if (human === "yes") {
+    let intro = document.querySelector(".newby");
+    intro.innerHTML =
+      "Move the cursor over pictures to enlarge them and click on song titles to play the videos. ↡ Scroll down and have FUN!";
+  } else {
+    let intro = document.querySelector(".newby");
+    intro.innerHTML = "Well, then you know your way around. 🤣";
+  }
+}
+
+function redirect() {
+  alert("You will be directed to a new website");
+}
+
+let name = prompt("Welcome to my BTOB Fanpage! What is your name?");
+let greeting = document.querySelector(".greet");
+greeting.innerHTML = "Hi " + name + ", nice of you to drop by!";
+
+let entryButton = document.querySelector(".firstTime");
+entryButton.addEventListener("click", enterHere);
+
+let buyButton = document.querySelector(".buyIt");
+buyButton.addEventListener("click", redirect);
